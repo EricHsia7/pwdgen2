28dd8f7c9d7d14c5d30f13d6e915f4c1ff8a0696
Eric <54091924+EricHsia7@users.noreply.github.com>
2023-07-26 06:47:20 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
