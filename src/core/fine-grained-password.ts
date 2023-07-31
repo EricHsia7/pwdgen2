cf9ce3f0a843f16c6ed53c53ceb3b97cb9e8a533
Eric <54091924+EricHsia7@users.noreply.github.com>
2023-07-31 17:51:00 +0800

    fix grammar errors

--- src/core/fine-grained-password.ts
+++ src/core/fine-grained-password.ts
@@ -1,8 +1,8 @@
-import { LS, searchItemsbyname } from './storage'
-import utilities from './utilities'
+import { LS, searchItemsbyname } from './storage';
+import utilities from './utilities';
 var _ = require('lodash');
 
-const pwd_pattern_default: object = [
+const pwd_pattern_default = [
   {
     "pattern_name": "Strong",
     "pattern_icon": "key",
@@ -130,33 +130,37 @@ const pwd_pattern_default: object = [
       }
     ]
   }
-]
+];
 
-//function to get all the patterns saved in the local storage
+// Function to get all the patterns saved in the local storage
 function listPatterns() {
-  var list = searchItemsbyname('pwdgen2_pattern_b_')
-  var list_len = list.length
-  var result = []
+  var list = searchItemsbyname('pwdgen2_pattern_b_');
+  var list_len = list.length;
+  var result = [];
   for (var p = 0; p < list_len; p++) {
     if (LS.hasOwnProperty(list[p])) {
-      result.push(JSON.parse(String(LS.getItem(list[p]))))
+      result.push(JSON.parse(String(LS.getItem(list[p]))));
     }
   }
-  return result
+  return result;
 }
-const pwd_pattern_custom: object = []
-//function to get the default and saved patterns
-const getPatterns = function (): object {
-  fine_grained_password.pwd_pattern_custom = fine_grained_password.listPatterns()
-  return pwd_pattern_default.concat(fine_grained_password.pwd_pattern_custom)
-}
-//function to generate string depending on the pattern 
-function generate(options: object, mode: string): string | object {
-  //use lodash to clone the options (pattern) to avoid syncing with the original list declared above → make the declared pattern usable repeatedly
-  const pattern: object = _.cloneDeep(options);
-  const original_pattern: object = _.cloneDeep(options)
-  //production mode: only return the results
-  //editor mode: return the results, component objects, and error messages
+
+const pwd_pattern_custom = [];
+
+// Function to get the default and saved patterns
+const getPatterns = function () {
+  fine_grained_password.pwd_pattern_custom = fine_grained_password.listPatterns();
+  return pwd_pattern_default.concat(fine_grained_password.pwd_pattern_custom);
+};
+
+// Function to generate a string depending on the pattern
+function generate(options, mode) {
+  // Use lodash to clone the options (pattern) to avoid syncing with the original list declared above → make the declared pattern usable repeatedly
+  const pattern = _.cloneDeep(options);
+  const original_pattern = _.cloneDeep(options);
+
+  // Production mode: only return the results
+  // Editor mode: return the results, component objects, and error messages
   if (mode === 'production') {
     var d: string = ""
   }
@@ -165,230 +169,236 @@ function generate(options: object, mode: string): string | object {
   }
 
   const get_chars_from_regex = function (regex) {
-    //generate a long string
+    // Generate a long string
     const input: string = String.fromCharCode(...Array.from({ length: Math.pow(2, 16) }, (_, i) => i));
-    //use the inputed regular expression to select matched characters
+    // Use the input regular expression to select matched characters
     const matches: any = String(input).match(regex);
-    //join the items in the array
+    // Join the items in the array
     const chars: string = matches ? matches.join('') : '';
-    //return the joined string
-    return chars
-  }
+    // Return the joined string
+    return chars;
+  };
+
   const pattern_len: number = pattern.length
   for (var e = 0; e < pattern_len; e++) {
-    var this_item = pattern[e]
-    var this_content = this_item[this_item['type']]
-    var result = ''
+    var this_item = pattern[e];
+    var this_content = this_item[this_item['type']];
+    var result = '';
+
     if (this_item['type'] === "regex") {
-      //check if the regular expression is full (has expression and flags)
+      // Check if the regular expression is full (has expression and flags)
       var this_content_matches = this_content.match(/^\/(.*)\/([a-z]*)$/i);
       var string: string = ''
-      //get the character source/sample
-      var chars = get_chars_from_regex(new RegExp(this_content_matches[1], this_content_matches[2])).split('')
+      // Get the character source/sample
+      var chars = get_chars_from_regex(new RegExp(this_content_matches[1], this_content_matches[2])).split('');
       for (var r = 0; r < this_item['quantity']; r++) {
-        //choose a character from the source
-        var random_index = Math.round((chars.length - 1) * Math.random())
+        // Choose a character from the source
+        var random_index: number = Math.round((chars.length - 1) * Math.random());
 
-        //put the character to the end of the result
-        string += chars[random_index]
+        // Put the character to the end of the result
+        string += chars[random_index];
 
-        //if the configuration tells that repeating is not allowed, strike/remove the character from the source to avoid using repeatedly
+        // If the configuration tells that repeating is not allowed, strike/remove the character from the source to avoid using repeatedly
         if (!this_item.repeat) {
-          chars.splice(random_index, 1)
+          chars.splice(random_index, 1);
         }
-
       }
-      result = String(string)
+      result = String(string);
     }
+
     if (this_item['type'] === "string") {
-      //directly put the string to the end of the result
-      result = String(this_content)
+      // Directly put the string to the end of the result
+      result = String(this_content);
     }
+
     if (this_item['type'] === "list") {
       for (var r = 0; r < this_item['quantity']; r++) {
-        //choose an item from the list
-        var random_index = Math.round((this_content.length - 1) * Math.random())
-        //put the content of the item to the end of the result
-        result += this_content[random_index]
+        // Choose an item from the list
+        var random_index: number = Math.round((this_content.length - 1) * Math.random());
+        // Put the content of the item to the end of the result
+        result += this_content[random_index];
         if (!this_item.repeat) {
-          //if the configuration tells that repeating is not allowed, strike/remove the item from the list to avoid using repeatedly
-          this_content.splice(random_index, 1)
+          // If the configuration tells that repeating is not allowed, strike/remove the item from the list to avoid using repeatedly
+          this_content.splice(random_index, 1);
         }
       }
     }
+
     if (this_item['type'] === "group") {
-      //use recursive way to process the components in a group
-      result = fine_grained_password.generate(this_content, 'production')
-      //carry out the actions
+      // Use recursive way to process the components in a group
+      result = fine_grained_password.generate(this_content, 'production');
+      // Carry out the actions
       if (this_item.hasOwnProperty('actions')) {
-        var actions = this_item['actions']
-        var actions_len = actions.length
+        var actions = this_item['actions'];
+        var actions_len = actions.length;
         for (var j = 0; j < actions_len; j++) {
           if (actions[j] === 'shuffle') {
-            result = utilities.shuffleSelf(result.split(''), result.length).join('')
+            result = utilities.shuffleSelf(result.split(''), result.length).join('');
             continue;
           }
         }
       }
     }
+
     if (mode === 'production') {
-      d += result
+      d += result;
     }
     if (mode === 'editor') {
-      d.push({ result: result, component: original_pattern[e] })
+      d.push({ result: result, component: original_pattern[e] });
     }
   }
-  return d
+  return d;
 }
 
-function checkPatternQualification(pattern: object): object {
-  var json = _.cloneDeep(pattern)
-  var result = 1
-  var errors: Array = []
-  const omitobject = function (object: object): string {
-    var obj = _.cloneDeep(object)
+function checkPatternQualification(pattern) {
+  var json = _.cloneDeep(pattern);
+  var result = 1;
+  var errors: Array = [];
+
+  const omitobject = function (object) {
+    var obj = _.cloneDeep(object);
     if (typeof object === 'object' && !Array.isArray(object)) {
       for (var w in obj) {
         if (typeof obj[w] === 'object' && !Array.isArray(obj[w])) {
-          obj[w] = '{...}'
+          obj[w] = '{...}';
         }
         if (typeof obj[w] === 'object' && Array.isArray(obj[w])) {
-          obj[w] = '[...]'
+          obj[w] = '[...]';
         }
       }
-      return JSON.stringify(obj)
+      return JSON.stringify(obj);
     }
-  }
-  const check_hasOwnProperty = function (object: object, property: string): boolean {
+  };
+
+  const check_hasOwnProperty = function (object, property) {
     if (typeof object === 'object') {
       if (object.hasOwnProperty(property)) {
-        return 1
+        return 1;
       }
       else {
-        errors.push(`The property "${property}" was not found in ${omitobject(object)}.`)
-        return 0
+        errors.push(`The property "${property}" was not found in ${omitobject(object)}.`);
+        return 0;
       }
     }
-    errors.push(`Cannot check the property "${property}" due to type error of ${String(object)}.`)
-    return 0
-  }
-  var check = function (object: object) {
-    var result = 1
-    result *= check_hasOwnProperty(object, 'type')
-    var type = object['type']
+    errors.push(`Cannot check the property "${property}" due to a type error of ${String(object)}.`);
+    return 0;
+  };
+
+  const check = function (object) {
+    var result = 1;
+    result *= check_hasOwnProperty(object, 'type');
+    var type = object['type'];
     if (type === 'string' || type === 'regex' || type === 'list' || type === 'group') {
-      result *= check_hasOwnProperty(object, object['type'])
-    }
-    else {
-      errors.push(`The type "${type}" in ${omitobject(object)} was not supported at this time.`)
-      result *= 0
+      result *= check_hasOwnProperty(object, object['type']);
+    } else {
+      errors.push(`The type "${type}" in ${omitobject(object)} was not supported at this time.`);
+      result *= 0;
     }
     if (type === 'regex' || type === 'list') {
-      result *= check_hasOwnProperty(object, 'quantity')
-      result *= check_hasOwnProperty(object, 'repeat')
+      result *= check_hasOwnProperty(object, 'quantity');
+      result *= check_hasOwnProperty(object, 'repeat');
       if (!(typeof object['quantity'] === 'number')) {
-        errors.push(`Type of the property "quantity" in ${omitobject(object)} is not number.`)
-        result *= 0
+        errors.push(`Type of the property "quantity" in ${omitobject(object)} is not a number.`);
+        result *= 0;
       }
       if (!(typeof object['repeat'] === 'boolean')) {
-        errors.push(`Type of the property "repeat" in ${omitobject(object)} is not boolean (true or false).`)
-        result *= 0
+        errors.push(`Type of the property "repeat" in ${omitobject(object)} is not boolean (true or false).`);
+        result *= 0;
       }
     }
     if (type === 'list') {
       if (typeof object['list'] === 'object' && Array.isArray(object['list'])) {
-        var list = object['list']
-        var list_len = list.length
+        var list = object['list'];
+        var list_len: number = list.length;
         for (var e = 0; e < list_len; e++) {
           if (!(typeof list[e] === 'string')) {
-            errors.push(`Type of the item ${e} in the list of ${omitobject(object)} is not string.`)
-            result *= 0
+            errors.push(`Type of the item ${e} in the list of ${omitobject(object)} is not a string.`);
+            result *= 0;
           }
         }
-      }
-      else {
-        errors.push('Cannot get item due to type error or property not existing.')
-        result *= 0
+      } else {
+        errors.push('Cannot get item due to type error or property not existing.');
+        result *= 0;
       }
     }
     if (type === 'group') {
       if (typeof object['actions'] === 'object' && Array.isArray(object['actions'])) {
-        var actions = object['actions']
-        var actions_len = actions.length
+        var actions = object['actions'];
+        var actions_len: number = actions.length;
         for (var e = 0; e < actions_len; e++) {
           if (!(typeof actions[e] === 'string')) {
-            errors.push(`An item in actions must be string.`)
-            result *= 0
-          }
-          else {
+            errors.push(`An item in actions must be a string.`);
+            result *= 0;
+          } else {
             if (!(actions[e] === 'shuffle')) {
-              errors.push(`Cannot use ${actions[e]} at this time due to not supportted value.`)
-              result *= 0
+              errors.push(`Cannot use ${actions[e]} at this time due to an unsupported value.`);
+              result *= 0;
             }
           }
         }
       }
       if (typeof object['group'] === 'object' && Array.isArray(object['group'])) {
-        var group = object['group']
-        var group_len = group.length
-        for (var e = 0; e < group_len; e++) {
-          result *= check(group[e])
+        var group = object['group'];
+        var group_len: number = group.length;
+        for (vat e = 0; e < group_len; e++) {
+          result *= check(group[e]);
         }
       }
     }
     if (type === "regex") {
       if (typeof object['regex'] === 'string') {
         if (!object['regex'].match(/^\/(.*)\/([a-z]*)$/i)) {
-          errors.push(`The regex in ${JSON.stringify(object)} is invalid on formats.`)
-          result *= 0
+          errors.push(`The regex in ${JSON.stringify(object)} is invalid on formats.`);
+          result *= 0;
         }
       } else {
-        errors.push(`The type of the property "regex" in ${omitobject(object)} is not string.`)
-        result *= 0
+        errors.push(`The type of the property "regex" in ${omitobject(object)} is not a string.`);
+        result *= 0;
       }
     }
     if (result === 1) {
-      return true
+      return true;
     }
     else {
-      return false
+      return false;
     }
-  }
+  };
 
   if (typeof json === 'object') {
-    result *= check_hasOwnProperty(json, 'pattern_name')
-    result *= check_hasOwnProperty(json, 'pattern_icon')
-    result *= check_hasOwnProperty(json, 'pattern')
+    result *= check_hasOwnProperty(json, 'pattern_name');
+    result *= check_hasOwnProperty(json, 'pattern_icon');
+    result *= check_hasOwnProperty(json, 'pattern');
     if (!(typeof json['pattern_name'] === 'string')) {
-      errors.push(`Type of the property "pattern_name" in ${omitobject(json)} is not string.`)
-      result *= 0
+      errors.push(`Type of the property "pattern_name" in ${omitobject(json)} is not a string.`);
+      result *= 0;
     }
     if (!(typeof json['pattern_icon'] === 'string')) {
-      errors.push(`Type of the property "pattern_icon" in ${omitobject(json)} is not string.`)
-      result *= 0
+      errors.push(`Type of the property "pattern_icon" in ${omitobject(json)} is not a string.`);
+      result *= 0;
     }
     if (typeof json['pattern'] === 'object' && Array.isArray(json['pattern'])) {
-      var pattern = json['pattern']
-      var pattern_len = pattern.length
+      var pattern = json['pattern'];
+      var pattern_len: number = pattern.length;
       for (var i = 0; i < pattern_len; i++) {
-        result *= check(pattern[i])
+        result *= check(pattern[i]);
       }
-    } else {
-      errors.push(`Type of the property "pattern" in ${omitobject(json)} is not array.`)
-      result *= 0
+    }
+    else {
+      errors.push(`Type of the property "pattern" in ${omitobject(json)} is not an array.`);
+      result *= 0;
     }
   }
   else {
-    errors.push(`Type of the thing you want to check is not object.`)
-    result *= 0
+    errors.push(`Type of the thing you want to check is not an object.`);
+    result *= 0;
   }
   if (result === 1) {
-    result = true
+    result = true;
   }
   else {
-    result = false
+    result = false;
   }
-  return { errors: errors, result: result }
+  return { errors: errors, result: result };
 }
 
 window.fine_grained_password = {
@@ -397,6 +407,6 @@ window.fine_grained_password = {
   listPatterns,
   getPatterns,
   checkPatternQualification
-}
+};
 
 export default window.fine_grained_password
