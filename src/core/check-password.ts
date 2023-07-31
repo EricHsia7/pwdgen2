6e88c73e6b2378b9280967d2b4174f2dc9a836c1
Eric <54091924+EricHsia7@users.noreply.github.com>
2023-07-31 17:08:55 +0800

    fix grammar erros

--- src/core/check-password.ts
+++ src/core/check-password.ts
@@ -1,139 +1,139 @@
-import words_list from './words-list'
-import { LS } from './storage'
+import words_list from './words-list';
+import { LS } from './storage';
 
 export function checkCommonWordPatterns(string): number {
   if (!words_list.loaded) {
-    words_list.getWordsList()
-    return 0
+    words_list.getWordsList();
+    return 0;
   }
-  string = string.toLowerCase()
-  var len = string.length
+  string = string.toLowerCase();
+  var len = string.length;
 
-  var matched = []
+  var matched = [];
   for (var i = 0; i < len; i++) {
-    //narrow down range by prefixes and pre-distributed index.
+    // Narrow down the range by prefixes and pre-distributed index.
     if (words_list.c.hasOwnProperty(string.substring(i, i + 3))) {
-      var arr = words_list.c[string.substring(i, i + 3)].slice()
-      var arr_len = arr.length
-      //check the string if it includs a word in the range
+      var arr = words_list.c[string.substring(i, i + 3)].slice();
+      var arr_len = arr.length;
+      // Check if the string includes a word in the range.
       for (var r = 0; r < arr_len; r++) {
         if (string.indexOf(arr[r].trim()) !== -1 && matched.indexOf(arr[r]) < 0) {
-          matched.push(arr[r])
+          matched.push(arr[r]);
         }
       }
     }
   }
-  //sort the matched list by length from long to short to avoid counting subsets in a series of a root word repeatedly
-  //For example, "suitable" includes "suit" and "able"
+  // Sort the matched list by length from long to short to avoid counting subsets in a series of a root word repeatedly.
+  // For example, "suitable" includes "suit" and "able".
   matched.sort(function (a, b) {
-    return b.length - a.length
-  })
-  var matched_len = matched.length
+    return b.length - a.length;
+  });
+  var matched_len = matched.length;
   for (var g = 0; g < matched_len; g++) {
-    string = string.replaceAll(matched[g].trim(), '')
+    string = string.replaceAll(matched[g].trim(), '');
   }
-  //calculate the length proportion of filtered string and original string 
-  //return a number in the range from 0 to 1
-  return string.length / len
+  // Calculate the length proportion of the filtered string and the original string.
+  // Return a number in the range from 0 to 1.
+  return string.length / len;
 }
 
 export function checkPassword(string, cache, id) {
   var getPoints = function (string) {
-    //Use the stats to get a function
-    //1. Generate a plenty of strong passwords using the genrator
-    //2. Use "moved character length/distance between original string and sorting by unicode version" to calculate the points
-    //3. Create a chart to calculate the count
-    //4. Use the count as the weight of the final points
-    //5. Create a function (randomnessX) that load up various relationships between original points and final points from 0 to 100
-    const randomnessX = function (x) { return Math.min(100, Math.max(0, (8.31 - 1.89 * x + 0.0743 * Math.pow(x, 2) - 5.91 * Math.pow(10, -4) * Math.pow(x, 3)) / 35.89 * 100)) }
-    //Use the stats to get a formula (length)
-    var length: number = -42.7 + 29.7 * Math.log(string.length);
-    var arr: Array = string.split('')
-    var arr_len: number = arr.length
-    var randomness: number = 0
-    var n_arr: Array = []
-    var json: object = {}
-    var t: number[] = []
+    // Use the stats to get a function
+    // 1. Generate plenty of strong passwords using the generator.
+    // 2. Use "moved character length/distance between the original string and sorting by the unicode version" to calculate the points.
+    // 3. Create a chart to calculate the count.
+    // 4. Use the count as the weight of the final points.
+    // 5. Create a function (randomnessX) that loads up various relationships between original points and final points from 0 to 100.
+    const randomnessX = function (x) { return Math.min(100, Math.max(0, (8.31 - 1.89 * x + 0.0743 * Math.pow(x, 2) - 5.91 * Math.pow(10, -4) * Math.pow(x, 3)) / 35.89 * 100)); };
+    // Use the stats to get a formula (length).
+    var length = -42.7 + 29.7 * Math.log(string.length);
+    var arr = string.split('');
+    var arr_len = arr.length;
+    var randomness = 0;
+    var n_arr = [];
+    var json = {};
+    var t = [];
     for (var w = 0; w < arr_len; w++) {
-      //create an array to log their content, index, and unicode.
-      n_arr.push({ c: arr[w], i: w, u: String(arr[w]).charCodeAt(0) })
-      //create an object to calculate the proportion of same/used characters in the password → analyze the ingredient of the password
-      json['u_' + String(arr[w]).charCodeAt(0)] = json.hasOwnProperty('u_' + String(arr[w]).charCodeAt(0)) ? json['u_' + String(arr[w]).charCodeAt(0)] + 1 : 1
+      // Create an array to log their content, index, and unicode.
+      n_arr.push({ c: arr[w], i: w, u: String(arr[w]).charCodeAt(0) });
+      // Create an object to calculate the proportion of same/used characters in the password → analyze the ingredients of the password.
+      json['u_' + String(arr[w]).charCodeAt(0)] = json.hasOwnProperty('u_' + String(arr[w]).charCodeAt(0)) ? json['u_' + String(arr[w]).charCodeAt(0)] + 1 : 1;
     }
-    //sort the array by unicode
+    // Sort the array by unicode.
     n_arr.sort(function (a, b) {
-      return a.u - b.u
-    })
-    //calculate the total moving steps/distance of characters
+      return a.u - b.u;
+    });
+    // Calculate the total moving steps/distance of characters.
     for (var w = 0; w < arr_len; w++) {
-      randomness += Math.abs(w - n_arr[w].i)
+      randomness += Math.abs(w - n_arr[w].i);
     }
-    //convert total distance to points in the range from 0 to 100 and use the function to calculate the final points
-    randomness = randomnessX(randomness / n_arr.length / (string.length / 2) * 100)
+    // Convert total distance to points in the range from 0 to 100 and use the function to calculate the final points.
+    randomness = randomnessX(randomness / n_arr.length / (string.length / 2) * 100);
 
-    var repeat: number = 0
-    var repeat_len: number = 0
-    //calculate the proportion of same/used characters and length
+    var repeat = 0;
+    var repeat_len = 0;
+    // Calculate the proportion of same/used characters and length.
     for (var w in json) {
-      repeat += json[w]
-      repeat_len += 1
+      repeat += json[w];
+      repeat_len += 1;
     }
-    //calculate the final points in the range from 0 to 100
-    repeat = (1 - repeat / repeat_len / string.length) * 100
-    //calculate the proportion of types of characters
-    t.push(string.match(/[A-Z]*/gm).join('').length)
-    t.push(string.match(/[a-z]*/gm).join('').length)
-    t.push(string.match(/[0-9]*/gm).join('').length)
-    t.push(string.length - t[0] - t[1] - t[2])
+    // Calculate the final points in the range from 0 to 100.
+    repeat = (1 - repeat / repeat_len / string.length) * 100;
+    // Calculate the proportion of types of characters.
+    t.push(string.match(/[A-Z]*/gm).join('').length);
+    t.push(string.match(/[a-z]*/gm).join('').length);
+    t.push(string.match(/[0-9]*/gm).join('').length);
+    t.push(string.length - t[0] - t[1] - t[2]);
 
-    var t_avg: number = string.length / 4
-    var t_s: number = 0
+    var t_avg = string.length / 4;
+    var t_s = 0;
     for (var w = 0; w < 4; w++) {
-      t_s += Math.pow(t[w] - t_avg, 2)
+      t_s += Math.pow(t[w] - t_avg, 2);
     }
-    //calculate the standard deviation to get the degress of evening distribution/degress of richness on type
-    const ty: number = (1 - Math.min(Math.max(Math.sqrt(t_s / 4) / t_avg, 0), 1)) * 100
+    // Calculate the standard deviation to get the degrees of evening distribution/degrees of richness in type.
+    const ty = (1 - Math.min(Math.max(Math.sqrt(t_s / 4) / t_avg, 0), 1)) * 100;
 
-    var commonWords = checkCommonWordPatterns(string) * 100
+    var commonWords = checkCommonWordPatterns(string) * 100;
 
-    //combine the indicators with weights into single points
-    return [Math.min(Math.max(Math.floor((length * 3 + randomness * 1.7 + repeat * 2 + ty * 2 + commonWords * 2) / 10.7), 0), 100), Math.floor(length), Math.floor(randomness), Math.floor(repeat), Math.floor(ty), Math.floor(commonWords), t[0], t[1], t[2], t[3]]
-  }
-  //give colors and phrases depending on the points/levels
+    // Combine the indicators with weights into single points.
+    return [Math.min(Math.max(Math.floor((length * 3 + randomness * 1.7 + repeat * 2 + ty * 2 + commonWords * 2) / 10.7), 0), 100), Math.floor(length), Math.floor(randomness), Math.floor(repeat), Math.floor(ty), Math.floor(commonWords), t[0], t[1], t[2], t[3]];
+  };
+  // Give colors and phrases depending on the points/levels.
   var judgePoints = function (points) {
-    var phrase = ''
-    var r, g, b = 0
+    var phrase = '';
+    var r, g, b = 0;
     if (points >= 85) {
-      phrase = 'strong'
-      r = 52
-      g = 199
-      b = 89
+      phrase = 'strong';
+      r = 52;
+      g = 199;
+      b = 89;
     }
     if (points >= 60 && points < 85) {
-      phrase = 'average'
-      r = 242
-      g = 190
-      b = 0
+      phrase = 'average';
+      r = 242;
+      g = 190;
+      b = 0;
     }
     if (points >= 45 && points < 60) {
-      phrase = 'fragile'
-      r = 255
-      g = 149
-      b = 0
+      phrase = 'fragile';
+      r = 255;
+      g = 149;
+      b = 0;
     }
     if (points < 45) {
-      phrase = 'unsafe'
-      r = 255
-      g = 59
-      b = 48
+      phrase = 'unsafe';
+      r = 255;
+      g = 59;
+      b = 48;
     }
-    return [phrase, r, g, b]
-  }
+    return [phrase, r, g, b];
+  };
 
   var getReport = function (length_points, randomness_points, repeat_points, complexity_points, uniqueness_points, n1, n2, n3, n4) {
-    //give suggestions depending on the points of indicators
-    var points_summary = 5
-    // Initialize a set to store used instructions
+    // Give suggestions depending on the points of indicators.
+    var points_summary = 5;
+    // Initialize a set to store used instructions.
     var used_instructions = new Set();
     // Length Instructions
     if (length_points <= 25 && !used_instructions.has("length_short")) {
@@ -148,7 +148,7 @@ export function checkPassword(string, cache, id) {
     } else if (length_points >= 76 && !used_instructions.has("length_long_enough")) {
       var Length_Instructions = "The length of this password is long enough, but please keep awareness of the security of your accounts.";
       used_instructions.add("length_long_enough");
-      points_summary -= 1
+      points_summary -= 1;
     }
 
     // Randomness Instructions
@@ -164,7 +164,7 @@ export function checkPassword(string, cache, id) {
     } else if (randomness_points >= 76 && !used_instructions.has("randomness_random")) {
       var Randomness_Instructions = "The arrangement of characters in this password is entirely random, but please keep awareness of the security of your accounts.";
       used_instructions.add("randomness_random");
-      points_summary -= 1
+      points_summary -= 1;
     }
 
     // Repeat Instructions
@@ -180,7 +180,7 @@ export function checkPassword(string, cache, id) {
     } else if (repeat_points >= 76 && !used_instructions.has("repeat_hardly")) {
       var Repeat_Instructions = "In this password, the characters are hardly repeated, but please keep awareness of the security of your accounts.";
       used_instructions.add("repeat_hardly");
-      points_summary -= 1
+      points_summary -= 1;
     }
 
     // Complexity Instructions
@@ -196,26 +196,26 @@ export function checkPassword(string, cache, id) {
     } else if (complexity_points >= 76 && !used_instructions.has("complexity_complex")) {
       var Complexity_Instructions = `This password consists of ${n1} uppercases, ${n2} lowercases, ${n3} digits, and ${n4} other types of characters. The types of characters in this password are complex and rich. But please keep awareness of the security of your accounts.`;
       used_instructions.add("complexity_complex");
-      points_summary -= 1
+      points_summary -= 1;
     }
     // Uniqueness Instructions
     if (uniqueness_points <= 25 && !used_instructions.has("uniqueness_not_unique")) {
-      var Uniqueness_Instructions = `This password includes many common words. Suggest you remove the words from this password to make it more secure.`
+      var Uniqueness_Instructions = `This password includes many common words. Suggest you remove the words from this password to make it more secure.`;
       used_instructions.add("complexity_not_complex");
     } else if (uniqueness_points >= 26 && uniqueness_points <= 50 && !used_instructions.has("uniqueness_not_unique_enough")) {
-      var Uniqueness_Instructions = `This password includes some common words. Suggest you remove the words from this password to make it more secure.`
+      var Uniqueness_Instructions = `This password includes some common words. Suggest you remove the words from this password to make it more secure.`;
       used_instructions.add("complexity_not_complex_enough");
     } else if (uniqueness_points >= 51 && uniqueness_points <= 99 && !used_instructions.has("uniqueness_not_unique_enough_2")) {
-      var Uniqueness_Instructions = `This password includes common words. Suggest you remove the words from this password to make it more secure.`
+      var Uniqueness_Instructions = `This password includes common words. Suggest you remove the words from this password to make it more secure.`;
       used_instructions.add("complexity_not_complex_enough_2");
     } else if (uniqueness_points >= 100 && !used_instructions.has("uniqueness_unique")) {
-      var Uniqueness_Instructions = `This password doesn't include common words collected in our list. But please keep awareness of the security of your accounts.`
+      var Uniqueness_Instructions = `This password doesn't include common words collected in our list. But please keep awareness of the security of your accounts.`;
       used_instructions.add("complexity_complex");
-      points_summary -= 1
+      points_summary -= 1;
     }
 
     if (points_summary === 0) {
-      points_summary = 'no'
+      points_summary = 'no';
     }
 
     var report = `According to the points, this password has ${points_summary} problem${(points_summary > 1 ? 's' : '')}.
@@ -224,42 +224,40 @@ All you have to do is follow the instructions:
 2. ${Randomness_Instructions}
 3. ${Repeat_Instructions}
 4. ${Complexity_Instructions}
-5. ${Uniqueness_Instructions}`
+5. ${Uniqueness_Instructions}`;
 
-    return report
-  }
-  //use cache to avoid running report repeatedly and cause performance problems
+    return report;
+  };
+  // Use cache to avoid running the report repeatedly and cause performance problems.
   if (cache) {
-    var LS_key = `pwdgen2_saved_b_${id}`
-    var LS_cache_key = `pwdgen2_check_cache_b_${id}`
-    var nowtime = new Date().getTime()
+    var LS_key = `pwdgen2_saved_b_${id}`;
+    var LS_cache_key = `pwdgen2_check_cache_b_${id}`;
+    var nowtime = new Date().getTime();
     if (LS.hasOwnProperty(LS_cache_key)) {
-      var cached_result_points = parseInt(String(LS.getItem(LS_cache_key)).split(':')[1])
-      var cached_result_time_stamp = parseInt(String(LS.getItem(LS_cache_key)).split(':')[0])
-      var ju = judgePoints(cached_result_points)
+      var cached_result_points = parseInt(String(LS.getItem(LS_cache_key)).split(':')[1]);
+      var cached_result_time_stamp = parseInt(String(LS.getItem(LS_cache_key)).split(':')[0]);
+      var ju = judgePoints(cached_result_points);
       if ((nowtime - cached_result_time_stamp) < 43200000) {
-        return { 'points': cached_result_points, 'phrase': ju[0], 'color': { r: ju[1], g: ju[2], b: ju[3] }, 'details': null }
-      }
-      else {
-        LS.removeItem(LS_cache_key)
+        return { 'points': cached_result_points, 'phrase': ju[0], 'color': { r: ju[1], g: ju[2], b: ju[3] }, 'details': null };
+      } else {
+        LS.removeItem(LS_cache_key);
       }
     }
   }
-  var points = getPoints(string)
-  var ju = judgePoints(points[0])
-  var ju_length = judgePoints(points[1])
-  var ju_randomness = judgePoints(points[2])
-  var ju_repeat = judgePoints(points[3])
-  var ju_complexity = judgePoints(points[4])
-  var ju_uniqueness = judgePoints(points[5])
+  var points = getPoints(string);
+  var ju = judgePoints(points[0]);
+  var ju_length = judgePoints(points[1]);
+  var ju_randomness = judgePoints(points[2]);
+  var ju_repeat = judgePoints(points[3]);
+  var ju_complexity = judgePoints(points[4]);
+  var ju_uniqueness = judgePoints(points[5]);
   if (cache) {
-    LS.setItem(LS_cache_key, nowtime + ':' + points[0])
+    LS.setItem(LS_cache_key, nowtime + ':' + points[0]);
   }
   return {
     'points': points[0],
     'phrase': ju[0],
-    'color':
-    {
+    'color': {
       r: ju[1],
       g: ju[2],
       b: ju[3]
@@ -267,8 +265,7 @@ All you have to do is follow the instructions:
     'details': {
       len: {
         points: points[1],
-        color:
-          { r: ju_length[1], g: ju_length[2], b: ju_length[3] }
+        color: { r: ju_length[1], g: ju_length[2], b: ju_length[3] }
       },
       randomness: {
         points: points[2],
@@ -301,9 +298,8 @@ All you have to do is follow the instructions:
           g: ju_uniqueness[2],
           b: ju_uniqueness[3]
         }
-      }
-      ,
+      },
       report: getReport(points[1], points[2], points[3], points[4], points[5], points[6], points[7], points[8], points[9])
     }
-  }
+  };
 }
